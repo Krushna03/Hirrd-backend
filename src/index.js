1c852eb69d25cccb3c294a9cff125d842975b9f2
krushna <krushnasakhare965@gmail.com>
2024-10-16 12:51:24 +0530

    index file edited

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { app } from './src/app.js'
+import { app } from './app.js'
 import dotenv from 'dotenv'
-import connectDB from './src/DataBase/index.js'
+import connectDB from './DataBase/index.js'
 
 
 dotenv.config({
